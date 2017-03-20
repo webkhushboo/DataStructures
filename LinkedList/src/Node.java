33c461829f64e3d8f2d2bcf2a048619c2c6566b1
Khushboo Agrawal <khagrawa@adobe.com>
2017-03-20 14:29:48 +0530

    Initial commit with linked list program

---
+++ LinkedList/src/Node.java
@@ -0,0 +1,21 @@
+
+public class Node {
+
+	private int data ;
+	public Node next ;
+	
+	
+	public Node(){
+		next = null;
+		data =0;
+	}
+	
+	public Node(int data){
+		next = null;
+		this.data=data;
+	}
+	
+	public int getData(){
+		return data;
+	}
+}
